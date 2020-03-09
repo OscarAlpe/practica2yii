cb22ca6dedf801f3899c58c52b3974eb4c1fe2f1
Oscar Megía López <pfgytk@gmail.com>
2020-03-09 13:53:52 +0100

    Borrado el menú Home y ahora la pantalla principal apunta a Noticias

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -38,7 +38,6 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'Noticias', 'url' => ['/site/noticias']],
             ['label' => 'Articulos', 'url' => ['/site/articulos']],
             ['label' => 'Todo', 'url' => ['/site/todo']],
